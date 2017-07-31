20743d65b3facb305306bcbe8e25e40cc2db96f6
Jooong <joong937@gmail.com>
2017-07-31 16:59:51 +0900

    added GAN-Keras-MNIST
    and slightly changed some other codes, re-arranging files in this repository

--- CNN/vgg16.py
+++ CNN/vgg16.py
@@ -31,43 +31,43 @@ X = tf.placeholder(tf.float32,[None,224,224,3])
 
 # layer 1
 W1 = tf.Variable(tf.random_normal([3,3,3,32],stddev=0.01))    ; print(W1)  # 3*3 filter가 32개
-conv1 = tf.nn.conv2d(X, W1, strides=[1,1,1,1],padding="SAME")    ; print(conv1)#  224*224 size의 그림 * 32개
-relu1 = tf.nn.relu(conv1)   ; print(relu1)
-pool1 = tf.nn.max_pool(relu1, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool1) # 112*112의 그림 * 32개
+W1 = tf.nn.conv2d(X, W1, strides=[1,1,1,1],padding="SAME")    ; print(conv1)#  224*224 size의 그림 * 32개
+W1 = tf.nn.relu(W1)   ; print(relu1)
+W1 = tf.nn.max_pool(W1, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool1) # 112*112의 그림 * 32개
 
 # layer 2
 W2 = tf.Variable(tf.random_normal([3,3,32,64],stddev=0.01))    ; print(W2)  # 3*3 filter가 64개
-conv2 = tf.nn.conv2d(pool1, W2, strides=[1,1,1,1],padding="SAME")    ; print(conv2)#  112*112 size의 그림 * 64개
-relu2 = tf.nn.relu(conv2)   ; print(relu2)
-pool2 = tf.nn.max_pool(relu2, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool2) # 56*56의 그림 * 64개
+W2 = tf.nn.conv2d(W1, W2, strides=[1,1,1,1],padding="SAME")    ; print(conv2)#  112*112 size의 그림 * 64개
+W2 = tf.nn.relu(W2)   ; print(relu2)
+W2 = tf.nn.max_pool(W2, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool2) # 56*56의 그림 * 64개
 
 # layer 3
 W3 = tf.Variable(tf.random_normal([3,3,64,128],stddev=0.01))    ; print(W3)  # 3*3 filter가 128개
-conv3 = tf.nn.conv2d(pool2, W3, strides=[1,1,1,1],padding="SAME")    ; print(conv3)#  56*56 size의 그림 * 128개
-relu3 = tf.nn.relu(conv3)   ; print(relu3)
-pool3 = tf.nn.max_pool(relu3, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool3) # 28*28의 그림 * 128개
+W3 = tf.nn.conv2d(W2, W3, strides=[1,1,1,1],padding="SAME")    ; print(conv3)#  56*56 size의 그림 * 128개
+W3 = tf.nn.relu(W3)   ; print(relu3)
+W3 = tf.nn.max_pool(W3, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool3) # 28*28의 그림 * 128개
 
 # layer 4
 W4 = tf.Variable(tf.random_normal([3,3,128,256],stddev=0.01))    ; print(W4)  # 3*3 filter가 256개
-conv4 = tf.nn.conv2d(pool3, W4, strides=[1,1,1,1],padding="SAME")    ; print(conv4)#  28*28 size의 그림 * 256개
-relu4 = tf.nn.relu(conv4)   ; print(relu4)
-pool4 = tf.nn.max_pool(relu4, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool4) # 14*14의 그림 * 256개
+W4 = tf.nn.conv2d(W3, W4, strides=[1,1,1,1],padding="SAME")    ; print(conv4)#  28*28 size의 그림 * 256개
+W4 = tf.nn.relu(W4)   ; print(relu4)
+W4 = tf.nn.max_pool(W4, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool4) # 14*14의 그림 * 256개
 
 # layer 5
 W5 = tf.Variable(tf.random_normal([3,3,256,512],stddev=0.01))    ; print(W5)  # 3*3 filter가 512개
-conv5 = tf.nn.conv2d(pool4, W5, strides=[1,1,1,1],padding="SAME")    ; print(conv5)#  14*14 size의 그림 * 512개
-relu5 = tf.nn.relu(conv5)   ; print(relu5)
-pool5 = tf.nn.max_pool(relu5, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool5) # 7*7의 그림 * 512개
-flattened = tf.reshape(pool5, [-1, 7*7*512])
+W5 = tf.nn.conv2d(W4, W5, strides=[1,1,1,1],padding="SAME")    ; print(conv5)#  14*14 size의 그림 * 512개
+W5 = tf.nn.relu(W5)   ; print(relu5)
+W5 = tf.nn.max_pool(W5, ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME") ; print(pool5) # 7*7의 그림 * 512개
+flattened = tf.reshape(W5, [-1, 7*7*512])
 
 # fully connected
-dense1 = tf.layers.dense(flattened,4096,activation=tf.nn.relu)
-drop1 = tf.nn.dropout(dense1,0.5)
+D1 = tf.layers.dense(flattened,4096,activation=tf.nn.relu)
+D1 = tf.nn.dropout(D1,0.5)
 
-dense2 = tf.layers.dense(drop1,4096,activation=tf.nn.relu)
-drop2 = tf.nn.dropout(dense2,0.5)
+D2 = tf.layers.dense(D1,4096,activation=tf.nn.relu)
+D2 = tf.nn.dropout(D2,0.5)
 
-logit = tf.layers.dense(drop2,2,activation=tf.nn.softmax)
+logit = tf.layers.dense(D2,2,activation=tf.nn.softmax)
 
 
 sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
